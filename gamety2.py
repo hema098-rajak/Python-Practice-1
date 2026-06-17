87c68ed766941e458bf51532dcfd4236b27adbef
Hemlata Kumari <hemlatarajak850@gmail.com>
2026-06-17 15:37:46 +0530

    Address Chapter 5 practice questions

--- gamety2.py
+++ gamety2.py
@@ -11,3 +11,12 @@ secret_number = final_number / 2 - 5
 print("😲 Your secret number was", secret_number)
 
 print("✨ Math Magic Complete ✨")
+#2. 
+
+print("think of a magical letter ")
+print("add any vowels in it")
+print("now add a cosonant ")
+final_word = input("Enter your final word: ")
+secret_letter = final_word 
+print("your secret letter was : ", secret_letter )
+print("well done!")
